d2af921b7a7eac557abab12afd276259b5dedf7b
OSU UWRT <osu.uwrt@gmail.com>
2020-11-01 13:51:10 -0500

    fixed some python issues

--- src/riptide_states/big_rectangle_state.py
+++ src/riptide_states/big_rectangle_state.py
@@ -2,8 +2,8 @@
 
 from flexbe_core import EventState, Logger
 import rospy
-from darknet_ros_msg import BoundingBoxes
-
+import darknet_ros_msgs.msg._BoundingBoxes as BoundingBoxes
+from flexbe_core.proxy import ProxySubscriberCached
 from flexbe_core.proxy import ProxyPublisher
 from geometry_msgs.msg import PoseStamped
 
@@ -26,7 +26,7 @@ class BigRectangleState(EventState):
 	
 	def __init__(self, topic, threshold, time, target, timeout):
 		"""Constructor"""
-		super(BigRectangleState, self).__init__(outcomes=['Success', 'Failed'], output_keys=['x,y'])
+		super(BigRectangleState, self).__init__(outcomes=['Success', 'Failed'], output_keys=['x','y'])
 		self._threshold = threshold
 		self._target_time = time
 		self._start_time = rospy.Time.now()
@@ -34,19 +34,19 @@ class BigRectangleState(EventState):
 		self._topic = topic
 		self._target = target
 		#self._pub = ProxyPublisher({self._topic: PoseStamped})
-		self._sub = ProxySubscriber({self._topic: BoundingBoxes})
-		self.potential
+		self._sub = ProxySubscriberCached({self._topic: BoundingBoxes})
+		self._potential = []
 
 
 
 
-	def callback(bbox,userdata):
-		self.maxProbability = self.threshold
+	def callback(self,bbox,userdata):
+		self.maxProbability = self._threshold
 		for box in bbox.bounding_boxes:
 			if box.Class == self._target and box.probability >= self.maxProbability:
 				#Check if any previous bboxes are the same
 				found = False
-				for seenBox in self.potential:
+				for seenBox in self._potential:
 					if box.ymin <= seenBox.ymin +10 | box.ymin >= seenBox.ymin -10:
 						found = True
 						if bbox.bounding_boxes.header.stamp.nseq - seenBox.time >=self._target_time:
@@ -55,16 +55,17 @@ class BigRectangleState(EventState):
 							return 'Success'
 				#If the new box isn't on the potential list, add it.
 				if not found:
-					self.potential[len(potential)+1].box = box
-					self.potential[len(potential)+1].time = bbox.header.stamp.nseq
+					self._potential[len(self._potential)+1].box = box
+					self._potential[len(self._potential)+1].time = bbox.header.stamp.nseq
+
 		return 'Failed'
 
 
-	def execute(self, userdata):
+	def execute(self,userdata):
 		if self._sub.has_msg(self._topic):
 			msg = self._sub.get_last_msg(self._topic)
 			self._sub.remove_last_msg(self._topic)
-		if callback(msg,userdata) == 'Success':
+		if self.callback(msg,userdata) == 'Success':
 			return 'Success'
-		if self.start_time - rospy.Time.now() > self._timeout:
+		if self._start_time - rospy.Time.now() > self._timeout:
 			return 'Failed'
