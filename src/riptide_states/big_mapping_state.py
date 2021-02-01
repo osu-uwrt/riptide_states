b283c9660d68c30fc138018f3b9f7b3745a738b6
evmarx <esmarx1121@gmail.com>
2021-02-01 18:07:42 -0500

    state created to be used during search. Subscribes to mapping topic.

--- src/riptide_states/big_mapping_state.py
+++ src/riptide_states/big_mapping_state.py
@@ -6,7 +6,7 @@ from flexbe_core.proxy import ProxyActionClient
 from geometry_msgs.msg import PoseStamped
 
 
-class SubscriberState(EventState):
+class BigMappingState(EventState):
     """
 	Brings data in from mapping topic. 
     Subscribes to mapping topic.
@@ -18,9 +18,20 @@ class SubscriberState(EventState):
 	<= done							Pose has been published.
 
 	"""
-	def listener():
-        ProxySubscriber.init_node('listener', anonymous=True)
-		ProxySubscriber("mapping/gate", PoseWithCovarianceStamped, queue_size=1)
+	
+	def __init__(self, topic):
+		"""Constructor"""
+		super(BigMappingState, self).__init__(outcomes=['Success', 'Failure'],
+            input_keys=['angle'])
+		self._topic = topic
+		self.sub = ProxySubscriber("/puddles/mapping/gate", PoseWithCovarianceStamped, queue_size=1)
 
-    if __name__ == '__main__':
-        listener()			## do i need this w proxy?? I used rospy before so I used this to check the name. Does proxy work the same?
+	def execute(self, userdata):
+		if self.client.has_result(self._topic):
+			result = self.client.get_result(self._topic)
+			status = 'Success'       
+			return status
+
+
+
+	
