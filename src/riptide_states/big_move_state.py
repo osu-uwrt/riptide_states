510d6c15400d85de5c5388933edcf3bd39a61254
OSU UWRT <osu.uwrt@gmail.com>
2021-02-01 18:36:07 -0500

    Fixed move state

--- src/riptide_states/big_move_state.py
+++ src/riptide_states/big_move_state.py
@@ -2,9 +2,15 @@
 
 from flexbe_core import EventState, Logger
 import rospy
+import sys
 
 from flexbe_core.proxy import ProxyPublisher
+from flexbe_core.proxy import ProxySubscriberCached
 from geometry_msgs.msg import PoseStamped
+
+from moveit_commander import RobotCommander, roscpp_initialize, roscpp_shutdown
+from moveit_msgs.msg import RobotState
+from nav_msgs.msg import Odometry
 
 
 class BigMoveState(EventState):
@@ -19,17 +25,41 @@ class BigMoveState(EventState):
 
 	"""
 	
-	def __init__(self, topic):
+	def __init__(self, x,y,z,orientation):
 		"""Constructor"""
-		super(BigMoveState, self).__init__(outcomes=['done'],
-												input_keys=['pose'])
+		super(BigMoveState, self).__init__(outcomes=['done','failed'])
+		self.x = x
+		self.y = y
+		self.z = z
+		self.orientation = orientation
 
-		self._topic = topic
-		self._pub = ProxyPublisher({self._topic: PoseStamped})
+		self.loc_topic = '/Puddles/odometry/filtered'
+		self.sub = ProxySubscriberCached({self.loc_topic: Odometry})
+		
+
+		
+		
 
 
 	def execute(self, userdata):
 		return 'done'
 	
 	def on_enter(self, userdata):
-		self._pub.publish(self._topic, userdata.pose)
+		roscpp_initialize(sys.argv)
+		self.robot = RobotCommander()
+		msg = Odometry()
+		if self.sub.has_msg(self.loc_topic):
+			msg = self.sub.get_last_msg()
+			self.sub.remove_last_msg(self.loc_topic)
+		
+		self.x += msg.pose.pose.x
+		self.y += msg.pose.pose.y
+		self.z += msg.pose.pose.z 
+
+		# X, Y, Z, x, y, z, w
+		r = [self.x, self.y, self.z, 0, 0, 0, 1]
+
+		self.planner = self.robot.puddles_base
+		self.plan = self.planner.plan(r)
+
+		self.planner.execute(self.plan)
